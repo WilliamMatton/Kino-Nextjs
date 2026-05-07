4829052654cdaae42079696d7d5687b5b41da13f
William Matton <williammatton@gmail.com>
2026-05-07 13:23:00 +0200

    Clean up repo.

--- static/exampleAPIfetch.js
+++
@@ -1,9 +0,0 @@
-// Denna fil är endast ett exempel på hur vi kopplar vår egen kod
-// till vårt API genom en browser-fetch.
-
-function loadMovies() { // exempel för att ladda alla filmer
-  // länka till din resurs i movieAPI.js via http://localhost:5080/
-  const movies = fetch('http://localhost:5080/movies');
-}
-
-loadMovies(); // anropa funktionen när sidan har laddat
--- static/pagination.js
+++
@@ -1,4 +0,0 @@
-export function getReviewsWithPagination(allReviews, pageSize, pageNumber) {
-  const start = (pageNumber - 1) * pageSize;
-  return allReviews.slice(start, start + pageSize);
-}
