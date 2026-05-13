328777f04654f80197db772920951467e1c7ca82
William Matton <williammatton@gmail.com>
2026-05-13 15:27:58 +0200

    Remove code not belonging in this branch.

--- kino-next/src/app/api/reviews/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET() {
-  const reviews = [{
-    review: "Bra film"
-  }];
-  return NextResponse.json(reviews, { status: 200 });
-}
--- kino-next/src/app/movieIntro/page.tsx
+++ kino-next/src/app/movieIntro/page.tsx
@@ -1,16 +1,7 @@
-import kinoApiAdapter from "@/kinoApiAdapter";
-
-import MovieDetails from "@/components/movieIntro/MovieDetails";
-
 import "@/styles/movieIntro.scss";
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ id: number }> }) {
-  const { id } = await searchParams;
-
-  const res = await kinoApiAdapter.fetchMovie(id);
-  const movie = await res.json();
-
+export default async function Page() {
   return(
-    <MovieDetails movieData={movie} />
+    <h1>Detaljsida</h1>
   );
 }
