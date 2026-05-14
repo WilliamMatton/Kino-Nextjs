4fafc1dbdd7a7846149d24a1eae520f20d76feaf
maju0029 <maju0029@gmail.com>
2026-05-14 14:34:29 +0200

    installing ts-jest with config. Unittest for upcoming movies with mocked data.

--- kino-next/src/app/api/reviews/route.ts
+++ kino-next/src/app/api/reviews/route.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
+const SCREENINGS_API = 'https://plankton-app-xhkom.ondigitalocean.app/api/screenings';
 
+// Test api route
 export async function GET() {
   const reviews = [{
     review: "Bra film"
   }];
   return NextResponse.json(reviews, { status: 200 });
+}
+
+export async function getFirstFiveScreenings(id: string) {
+  const response = await fetch(`${SCREENINGS_API}?movieId=${id}`);
+
+  const { data } = await response.json();
+  return data.slice(0, 5);
 }
