622fdf0faeaef2707248c43ab913dbd139f7ef55
William Matton <williammatton@gmail.com>
2026-05-21 12:47:15 +0200

    Create test for fetching reviews.

---
+++ kino-next/src/app/test/reviews/getReviews.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from '@jest/globals';
+import cmsAdapter from '@/cmsAdapter';
+
+describe('getReviews()', () => {
+  it('tests review fetching', async () => {
+    return true;
+  });
+});
