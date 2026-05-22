746524165955eb82c1f8db2c9692537ea3f49676
William Matton <williammatton@gmail.com>
2026-05-22 11:49:55 +0200

    Update movieIntro page.

--- kino-next/src/app/movieIntro/page.tsx
+++ kino-next/src/app/movieIntro/page.tsx
@@ -1,22 +1,7 @@
 import "@/styles/movieIntro.scss"
-import { getFirstFiveScreenings, getReviewRating } from "../api/reviews/route";
 
-export default async function MovieIntro() {
-  const screenings = await getFirstFiveScreenings("movie-id");
+export default async function Page() {
   return(
-    <><h1>Detaljsida</h1><div>
-      <p>Movie rating: {getReviewRating("movie-id", 5)}</p>
-    </div>
-    <div>
-      <p>Kommande visningar:</p>
-      {
-        screenings.map((screening: any) => (
-          <p key={screening.id}>
-            {screening.attribute.start_time}
-          </p>
-        ))
-      }
-    </div>
-  </>
+    <h1>Detaljsida</h1>
   );
 }
