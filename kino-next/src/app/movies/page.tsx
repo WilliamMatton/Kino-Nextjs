9f2ba7702daace6aa523610406cc27cb18eeb1e7
mastr777 <mstrandberg777@gmail.com>
2026-05-25 13:47:10 +0200

    add some code to movies page

--- kino-next/src/app/movies/page.tsx
+++ kino-next/src/app/movies/page.tsx
@@ -1,2 +1,13 @@
 /* page for all movies */
 
+/* depending on what components to use   v */
+/* import Navbar from "@/components/Navbar"; */
+import "@/styles/moviespage.scss"
+
+export default function MoviesPage() {
+  return (
+      <main>
+            <h1>All movies shown</h1>
+      </main>
+  );
+}
