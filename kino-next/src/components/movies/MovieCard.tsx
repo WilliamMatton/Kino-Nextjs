a54214f447c8da289e11daf732262cf4ee7e7261
mastr777 <mstrandberg777@gmail.com>
2026-05-26 19:35:49 +0200

    refactor and make code for each card

--- kino-next/src/components/movies/MovieCard.tsx
+++ kino-next/src/components/movies/MovieCard.tsx
@@ -1,1 +1,61 @@
 /* code for each movie card */
+
+"use client";
+
+import { useRouter } from "next/navigation";
+
+type ApiMovie = {
+  id: number;
+  attributes?: {
+  title?: string;
+  intro?: string;
+  image?: {
+  url?: string;
+    };
+  };
+};
+
+type MovieCardProps = {
+  movie: ApiMovie;
+};
+
+export default function MovieCard({ movie }: MovieCardProps) {
+
+  const router = useRouter();
+  const attrs = movie.attributes || {};
+  const imageUrl = attrs.image?.url || "";
+  const title = attrs.title || "Titel saknas";
+  const intro = attrs.intro || "";
+
+  function handleClick() {
+
+    router.push(`/movieIntro?id=${movie.id}&page=1`);
+
+  }
+
+  return (
+    <article
+
+      className="card"
+      onClick={handleClick}
+      data-id={movie.id}
+      data-name={title}
+      data-img={imageUrl}
+    >
+      {imageUrl && (
+        <img src={imageUrl} alt={`Image for ${title}`} className="imageCard" />
+      )}
+
+      <div className="container">
+        <h3 className="cardTitle">{title}</h3>
+        {intro && <p className="cardInfo">{intro}</p>}
+      </div>
+
+    </article>
+  );
+}
+
+
+
+
+
