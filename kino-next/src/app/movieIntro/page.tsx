6ef52d08e2d0898c3cc36942f6cfd22bc362fd47
William Matton <williammatton@gmail.com>
2026-05-26 11:17:51 +0200

    Add necessary props to the ReviewContainer component and the movieIntro page, and fetch review data.

--- kino-next/src/app/movieIntro/page.tsx
+++ kino-next/src/app/movieIntro/page.tsx
@@ -2,8 +2,17 @@ import ReviewContainer from "@/components/movieIntro/ReviewContainer";
 
 import "@/styles/movieIntro.scss"
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    movieID: string;
+    page: string;
+  }>
+}) {
+  const searchParams = await props.searchParams;
+  const currentPage = Number(searchParams?.page) || 1;
+  const movieID = Number(searchParams?.movieID);
+  
   return(
-    <ReviewContainer />
+    <ReviewContainer movieID={movieID} page={currentPage} />
   );
 }
--- kino-next/src/components/movieIntro/ReviewContainer.tsx
+++ kino-next/src/components/movieIntro/ReviewContainer.tsx
@@ -4,7 +4,16 @@ import ReviewForm from "./ReviewForm";
 import ReviewList from "./ReviewList";
 import ReviewPagination from "./ReviewPagination";
 
-const ReviewContainer: FC = () => {
+type Props = {
+  movieID: number;
+  page: number;
+}
+
+const ReviewContainer: FC<Props> = async ({ movieID, page }) => {
+  const res = await fetch(`http://localhost:3000/api/reviews/${movieID}?page=${page}`);
+  const reviews = await res.json();
+  const totalPages = reviews.meta.pagination.pageCount;
+
   return (
     <section className="movieReviews">
       <h1 className="movieReviewsHeading">Recensioner</h1>
