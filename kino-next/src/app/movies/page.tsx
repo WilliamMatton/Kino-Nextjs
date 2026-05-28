6c99f5f7dccbb8305c982a8d73d3c8c898b5d933
mastr777 <mstrandberg777@gmail.com>
2026-05-28 15:01:28 +0200

    add a vanished semicolon

--- kino-next/src/app/movies/page.tsx
+++ kino-next/src/app/movies/page.tsx
@@ -3,7 +3,7 @@
 /* depending on what components to use   v */
 /* import Navbar from "@/components/Navbar"; */
 import MoviesList from "@/components/movies/MoviesList";
-import "@/styles/movies.scss"
+import "@/styles/movies.scss";
 
 export default function MoviesPage() {
   return (
