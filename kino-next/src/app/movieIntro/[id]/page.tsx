8f99428807c56e149b47eccb47e88058c14935b1
William Matton <williammatton@gmail.com>
2026-06-01 10:43:48 +0200

    Make the routing for the movie intro page dynamic instead of using query-params.

--- kino-next/src/app/movieIntro/[id]/page.tsx
+++ kino-next/src/app/movieIntro/[id]/page.tsx
@@ -3,15 +3,15 @@ import ReviewContainer from "@/components/movieIntro/ReviewContainer";
 import "@/styles/movieIntro.scss";
 
 export default async function Page(props: {
-  searchParams?: Promise<{
-    movieID: string;
-    page: string;
-  }>
+  params: Promise<{ id: string; }>,
+  searchParams?: Promise<{ page: string; }>
 }) {
+  const params = await props.params;
   const searchParams = await props.searchParams;
+
+  const movieID = Number(params.id);
   const currentPage = Number(searchParams?.page) || 1;
-  const movieID = Number(searchParams?.movieID);
-  
+
   return (
     <ReviewContainer movieID={movieID} page={currentPage} />
   );
