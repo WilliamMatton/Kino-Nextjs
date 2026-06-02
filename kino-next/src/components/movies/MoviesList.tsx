7453880337d49a22c4627653ebfae6d921814a8e
mastr777 <mstrandberg777@gmail.com>
2026-06-02 21:34:46 +0200

    Make MoviesList server component - for rendering issue

--- kino-next/src/components/movies/MoviesList.tsx
+++ kino-next/src/components/movies/MoviesList.tsx
@@ -1,59 +1,43 @@
 /* code for all movies to be rendered on movies page */
 
-"use client";
-
-import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 type ApiMovie = {
-
   id: number;
   attributes?: {
-  title?: string;
-  intro?: string;
-  image?: {
-  url?: string;
+    title?: string;
+    intro?: string;
+    image?: {
+      url?: string;
     };
   };
 };
 
-export default function MoviesList() {
+async function getMovies(): Promise<ApiMovie[]> {
+  const response = await fetch(
+    "https://plankton-app-xhkom.ondigitalocean.app/api/movies",
+    {
+      cache: "no-store",
+    },
+  );
 
-  const [movies, setMovies] = useState<ApiMovie[]>([]);
+  if (!response.ok) {
+    throw new Error("Could not fetch movies");
+  }
 
-  useEffect(() => {
-    async function fetchMovies() {
-      try {
-        const response = await fetch(
-          "https://plankton-app-xhkom.ondigitalocean.app/api/movies"
-        );
+  const apiData = await response.json();
+  return Array.isArray(apiData.data) ? apiData.data : [];
+}
 
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const apiData = await response.json();
-        const moviesData = Array.isArray(apiData.data) ? apiData.data : [];
-
-        setMovies(moviesData);
-
-      } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
-      }
-    }
-
-    fetchMovies();
-
-  }, []);
+export default async function MoviesList() {
+  const movies = await getMovies();
 
   return (
-
     <section id="cardsContainer" className="cardsContainer">
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </section>
-
   );
 }
 
